0fac9bcf2288f4d78df6f186f22504e0da4025ed
yaakov300 <yaacov300@gmail.com>
2015-10-22 19:22:04 +0300

    create image class
    for image manipulation

--- VisionMushnik/VisionMushnik/MainForm.h
+++ VisionMushnik/VisionMushnik/MainForm.h
@@ -34,6 +34,8 @@ namespace VisionMushnik {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,13 +50,24 @@ namespace VisionMushnik {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(110, 108);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(490, 305);
+			this->ClientSize = System::Drawing::Size(324, 302);
+			this->Controls->Add(this->button1);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
 			this->ResumeLayout(false);
