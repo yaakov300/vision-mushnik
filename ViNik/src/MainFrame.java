ca703ed4495b987fc66e9ee11f67ee9079a7a01b
yaakov300 <yaacov300@gmail.com>
2015-12-09 01:21:20 +0200

    add function that convert image to 2D matrix

--- ViNik/src/MainFrame.java
+++ ViNik/src/MainFrame.java
@@ -1,4 +1,4 @@
-import java.awt.EventQueue;
+import java.awt.EventQueue; 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -8,15 +8,20 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 import javax.swing.JLabel;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 
+import javax.imageio.ImageIO;
 
 public class MainFrame {
 
 	private JFrame frame;
 	private JLabel lblNewLabel;
-	
-	
+	private BufferedImage image;
+	int[][] imageArrayint;
+
 	// Launch the application.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -31,8 +36,8 @@ public class MainFrame {
 		});
 	}
 
-	
-	 //Create the application.	
+
+	//Create the application.	
 	public MainFrame() {
 		initialize();
 	}
@@ -59,21 +64,73 @@ public class MainFrame {
 				fc.setAcceptAllFileFilterUsed(true);
 				if(fc.showOpenDialog(null)==JFileChooser.APPROVE_OPTION)
 				{
-					
+
 					lblNewLabel.setText(fc.getSelectedFile().getAbsolutePath());
+					try
+					{
+						image = ImageIO.read(new File(fc.getSelectedFile().getAbsolutePath()));
+					}catch(IOException e)
+					{
+						System.out.println("ERROR read image");
+					}
+					imageArrayint = convertTo2DWithoutUsingGetRGB(image);
 				}
-				
-				
 			}
 		});
-		
+
 		//properties of stuff
 		btnOfenFile.setBounds(10, 11, 89, 23);
 		frame.getContentPane().add(btnOfenFile);
-		
+
 		lblNewLabel = new JLabel("No  file chosen");
 		lblNewLabel.setBounds(126, 15, 711, 14);
 		frame.getContentPane().add(lblNewLabel);
 	}
+
+	//this function return array[][] after convert from BufferedImage image: 
+	private static int[][] convertTo2DWithoutUsingGetRGB(BufferedImage image) {
+
+		final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		final int width = image.getWidth();
+		final int height = image.getHeight();
+		final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+		int[][] result = new int[height][width];
+		if (hasAlphaChannel) {
+			final int pixelLength = 4;
+			for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+				int argb = 0;
+				argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+				argb += ((int) pixels[pixel + 1] & 0xff); // blue
+				argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+				argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+				result[row][col] = argb;
+				col++;
+				if (col == width) {
+					col = 0;
+					row++;
+				}
+			}
+		} else {
+			final int pixelLength = 3;
+			for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+				int argb = 0;
+				argb += -16777216; // 255 alpha
+				argb += ((int) pixels[pixel] & 0xff); // blue
+				argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+				argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+				result[row][col] = argb;
+				col++;
+				if (col == width) 
+				{
+					col = 0;
+					row++;
+				}
+			}
+		}
+
+		return result;
+	}
+	
 }
- 
+
